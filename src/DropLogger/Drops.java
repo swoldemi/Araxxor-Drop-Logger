1571fd4b1ef7a199901c6892c25201ce5d64bd92
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-05 23:16:36 -0600

    Added Araxxi drop item names

---
+++ src/DropLogger/Drops.java
@@ -0,0 +1,20 @@
+package DropLogger;
+
+public class Drops {
+	final public static String[] arrow_pheromone = new String[]{"Araxyte pheromone", "Araxyte arrow"}; 
+	final public static String[] main_loot = new String[]{
+		"Coins", "Black dragonhide", "Crystal triskelion fragment 1", 
+		"Crystal triskelion fragment 2", "Crystal triskelion fragment 3",
+		"Sirenic scale", "Hydrix bolt tips", "Onyx bolts (e)",
+		"Rune platebody", "Uncut onyx", "Grimy avantoe", "Grimy dwarf weed",
+		"Grimy lantadyme", "Magic logs", "Yew logs", "Adamantite ore", "Coal",
+		"Adamantite ore", "Runite ore", "Dwarf weed seed", "Magic seed",
+		"Water talisman", "Battlestaff"};
+	final public static String[] food_potions = new String[]{
+		"Rocktail", "Saradomin brew flask (6)", "Super restore flask (6)",
+		"Overload flask (6)"};
+	final public static String[] unique_drops = new String[]{
+		"Spider leg bottom", "Spider leg middle", "Spider leg top",
+		"Araxxi's eye", "Araxxi's fang", "Araxxi's web"};
+	final public static String[] very_unique_drops = new String[]{"Starved ancient effigy", "Araxyte pet", "Araxyte pet"};
+}
