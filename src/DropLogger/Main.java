c078f28b65e15a0f062e2e003ffd845919dd716b
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-07 19:33:59 -0600

    Updated main to implement Logger

--- src/DropLogger/Main.java
+++ src/DropLogger/Main.java
@@ -7,14 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		String user_name = "Data_File";
 		DatabaseConnector connector = new DatabaseConnector();
-		
+		LoggerInterface user = null;
 		// Make an instance of the interface and attempt to make a table using the username provided on login
 		try {
-			LoggerInterface user = new LoggerInterface(user_name, connector);
+			user = new LoggerInterface(user_name, connector);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
+		// Begin the logger
+		try {
+			user.Logger();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
