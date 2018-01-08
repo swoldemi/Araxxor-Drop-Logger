7a016ce83e5c12718c48d6459a92bfab40676692
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-07 20:11:38 -0600

    Added variables to store the last row of each table

--- src/DropLogger/LoggerInterface.java
+++ src/DropLogger/LoggerInterface.java
@@ -2,9 +2,12 @@ package DropLogger;
 
 import java.sql.*;
 
+import javax.swing.JOptionPane;
+
 public class LoggerInterface {
 	private String user_name;
 	private DatabaseConnector connector;
+	
 	LoggerInterface(String name, DatabaseConnector connector) throws SQLException{
 		this.user_name = name;
 		this.connector = connector;
@@ -12,16 +15,41 @@ public class LoggerInterface {
 	}
 	//INSERT INTO data_file VALUES(1, 74, "12 Blue Charm", "12 Saradomin brew flask (6)", "220 Onyx bolts", null)
 	public void Logger() throws SQLException{
-		// First, get the current state of the database
+		String last_kill = null;
+		String pets = null;
+		
+		// First, get the current state of the main table
 		ResultSet current_state = connector.getTable(this.user_name);
 		while(current_state.next()){
 			System.out.println(current_state.getString("kill_number")
-					+ " " + current_state.getString("arrows_pheromone")
-					+ " " + current_state.getString("charms")
-					+ " " + current_state.getString("rocktails_sarabrews_overloads")
-					+ " " + current_state.getString("main_loot")
-					+ " " + current_state.getString("unique_drops") 
+					+ "|" + current_state.getString("arrows_pheromone")
+					+ "|" + current_state.getString("charms")
+					+ "|" + current_state.getString("rocktails_sarabrews_overloads")
+					+ "|" + current_state.getString("main_loot")
+					+ "|" + current_state.getString("unique_drops") 
 					);
+			last_kill = current_state.getString("kill_number")
+					+ "|" + current_state.getString("arrows_pheromone")
+					+ "|" + current_state.getString("charms")
+					+ "|" + current_state.getString("rocktails_sarabrews_overloads")
+					+ "|" + current_state.getString("main_loot")
+					+ "|" + current_state.getString("unique_drops");
 		}
+		
+		// Now, get the current state of the pets table
+		ResultSet current_pets_state = connector.getTable(this.user_name + "_pets");
+		while(current_pets_state.next()){
+			System.out.println(current_state.getString("araxyte_pet")
+					+ "|" + current_state.getString("barry")
+					+ "|" + current_state.getString("mallory"));
+			pets = current_state.getString("araxyte_pet")
+					+ "|" + current_state.getString("barry")
+					+ "|" + current_state.getString("mallory");
+		}
+		
+		while(true){
+			 
+		}
+		
 	}
 }
