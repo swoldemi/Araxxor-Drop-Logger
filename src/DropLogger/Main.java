71fee5ed8c8a05b3237b9813734dca3fc775895a
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-07 18:49:04 -0600

    Made interface constructor also create table

--- src/DropLogger/Main.java
+++ src/DropLogger/Main.java
@@ -5,12 +5,16 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) {
+		String user_name = "Data_File";
 		DatabaseConnector connector = new DatabaseConnector();
+		
+		// Make an instance of the interface and attempt to make a table using the username provided on login
 		try {
-			connector.makeTable("Data_File");
+			LoggerInterface user = new LoggerInterface(user_name, connector);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
