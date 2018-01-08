455aba5c1ee52f0c8fe3972f201f24af8dda7d27
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-07 20:09:42 -0600

    Added method to clean up username, now prompting for username

--- src/DropLogger/Main.java
+++ src/DropLogger/Main.java
@@ -2,12 +2,24 @@ package DropLogger;
 
 import java.sql.SQLException;
 
-public class Main {
+import javax.swing.JOptionPane;
 
+public class Main {
+	public static String sanitizeUsername(String user_name){
+		if(user_name.contains(" ")){
+			user_name.replace(" ", "_"); // replace spaces with underscores
+		}
+		return user_name;
+	}
 	public static void main(String[] args) {
-		String user_name = "Data_File";
 		DatabaseConnector connector = new DatabaseConnector();
 		LoggerInterface user = null;
+		
+		// Request user name for login
+		String user_name = JOptionPane.showInputDialog("Please enter your username to view your drop log.");
+		
+		// check for spaces in the username for SQL table creation
+		sanitizeUsername(user_name);
 		// Make an instance of the interface and attempt to make a table using the username provided on login
 		try {
 			user = new LoggerInterface(user_name, connector);
