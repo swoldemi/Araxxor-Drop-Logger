e6f38c7bea7e6a88b6a3801563f8d9ffefdc42c8
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-07 18:27:16 -0600

    Added error handle

--- src/DropLogger/Main.java
+++ src/DropLogger/Main.java
@@ -1,10 +1,16 @@
 package DropLogger;
 
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		DatabaseConnector connector = new DatabaseConnector();
-
+		try {
+			connector.makeTable("Data_File");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
