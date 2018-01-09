cc0efaab0e0cfa18d1bd862fd78aaf8323b876ea
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-09 06:45:13 -0600

    Removed swing thread in main()

--- src/DropLogger/Main.java
+++ src/DropLogger/Main.java
@@ -13,21 +13,21 @@ public class Main {
 	}
 	public static void main(String[] args) {
 		DatabaseConnector connector = new DatabaseConnector();
-		LoggerInterface user = null;
 		
 		// Request user name for login
 		String user_name = JOptionPane.showInputDialog("Please enter your username to view your drop log.");
 		
 		// check for spaces in the username for SQL table creation
 		sanitizeUsername(user_name);
-		// Make an instance of the interface and attempt to make a table using the username provided on login
+		
+		// Begin the logger
+    	LoggerInterface user = null;
+    	// Make an instance of the interface and attempt to make a table using the username provided on login
 		try {
 			user = new LoggerInterface(user_name, connector);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		// Begin the logger
 		try {
 			user.Logger();
 		} catch (SQLException e) {
