17587642292440324b7b85037a86c16bb5a2b2d1
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-09 18:59:49 -0600

    Update comments and renamed some variables

--- src/DropLogger/LoggerTable.java
+++ src/DropLogger/LoggerTable.java
@@ -12,16 +12,21 @@ import javax.swing.JTable;
 public class LoggerTable extends JPanel{
 	
 	LoggerTable(Object[][] table_contents){
+		// Define the default GridLayout in the parent class
 		super(new GridLayout(1,0));
+		
+		// Define column_names
 		String[] column_names = {"Kill #", "Arrows/Pheromone", "Charms", "Food/Potions", "Loot", "Unique Loot"};
+		
+		// Create the JTable
 		final JTable table = new JTable(table_contents, column_names);
 		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		table.setFillsViewportHeight(true);
 
-		//Create the scroll pane and add the table to it.
-		JScrollPane scrollPane = new JScrollPane(table);
+		//Create the scroll pane and add the table to it
+		JScrollPane scroll_pane = new JScrollPane(table);
 
-		//Add the scroll pane to this panel.
-    	add(scrollPane);
+		//Add the scroll pane to this panel
+    	add(scroll_pane);
 	}		
 }
