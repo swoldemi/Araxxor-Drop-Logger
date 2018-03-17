f204cb61cac071817b67c000876c3f1dd1c69f42
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-16 19:36:00 -0500

    Deleted package to replace with reversed domain name format

--- src/araxxorDropLogger/DatabaseCredentials.java
+++
@@ -1,7 +0,0 @@
-package araxxorDropLogger;
-
-public class DatabaseCredentials {
-	final public static String url = "jdbc:mysql://localhost:3306/droplogger?autoReconnect=true&useSSL=false";
-	final public static String username = "root";
-	final public static String password = "araxxorserver";
-}
